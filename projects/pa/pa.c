7828540e9fc610ad2a91d58b8215ccd4beae8b25
yymin1022 <yymin1022@gmail.com>
2023-05-24 05:26:45 +0000

    Initialized Project

---
+++ projects/pa/pa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "threads/init.h"
+#include "threads/malloc.h"
+#include "threads/palloc.h"
+#include "threads/thread.h"
+#include "projects/pa/pa.h"
+
+void run_patest(char **argv)
+{   
+    /// TODO: make your own test
+    
+    palloc_get_status(0);
+
+    while (1) {
+        timer_msleep(1000);
+    }
+}
---
+++ projects/pa/pa.h
@@ -0,0 +1,6 @@
+#ifndef __PROJECTS_PROJECT2_PA_H__
+#define __PROJECTS_PROJECT2_PA_H__
+
+void run_patest(char **argv);
+
+#endif /* __PROJECTS_PROJECT2_PA_H__ */
