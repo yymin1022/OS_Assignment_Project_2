b4a0017e45a717ef6dc15ebf832f9c2aa1765cec
yymin1022 <yymin1022@gmail.com>
2023-06-02 06:14:32 +0000

    Update thread.c : Added Thread Ready Lists for MFQ

--- threads/thread.c
+++ threads/thread.c
@@ -22,7 +22,10 @@
 
 /* List of processes in THREAD_READY state, that is, processes
    that are ready to run but not actually running. */
-static struct list ready_list;
+static struct list ready_list_fq0;
+static struct list ready_list_fq1;
+static struct list ready_list_fq2;
+static struct list ready_list_fq3;
 
 /* List of all processes.  Processes are added to this list
    when they are first scheduled and removed when they exit. */
@@ -94,7 +97,7 @@ thread_init (void)
   ASSERT (intr_get_level () == INTR_OFF);
 
   lock_init (&tid_lock);
-  list_init (&ready_list);
+  list_init (&ready_list_fq0);
   list_init (&all_list);
   list_init (&sleep_list);
 
@@ -242,7 +245,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_push_back (&ready_list_fq0, &t->elem);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -370,7 +373,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_push_back (&ready_list_fq0, &cur->elem);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -553,10 +556,10 @@ alloc_frame (struct thread *t, size_t size)
 static struct thread *
 next_thread_to_run (void) 
 {
-  if (list_empty (&ready_list))
+  if (list_empty (&ready_list_fq0))
     return idle_thread;
   else
-    return list_entry (list_pop_front (&ready_list), struct thread, elem);
+    return list_entry (list_pop_front (&ready_list_fq0), struct thread, elem);
 }
 
 /* Completes a thread switch by activating the new thread's page
